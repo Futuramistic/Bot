e2b9a72257110780f62cd5d3fd7c9fda86ef86f5
Futuramistic <32736399+Futuramistic@users.noreply.github.com>
2018-02-18 08:42:45 +0000

    "BOOOOT"

--- hello_bot.py
+++ hello_bot.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 #  -*- coding: utf-8 -*-
 import csv
+import random
 # 3rd party imports ------------------------------------------------------------
 from flask import Flask, request
 from ciscosparkapi import CiscoSparkAPI, Webhook
+from collections import defaultdict
 
 # local imports ----------------------------------------------------------------
 from helpers import (read_yaml_data,
@@ -11,19 +13,23 @@ from helpers import (read_yaml_data,
                      find_webhook_by_name,
                      delete_webhook, create_webhook)
 
-
 flask_app = Flask(__name__)
 spark_api = None
-interaction = {}
+interaction = defaultdict(list)
+response = defaultdict(list)
+used = []
+query = []
+type = []
 with open('interactions.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for row in readCSV:
         key = row[0]
         action = row[1]
-        interaction[key]=action
+        solution = row[2]
+        interaction[key].append(action)
+        response[action].append(solution)
 
 @flask_app.route('/sparkwebhook', methods=['POST'])
-
 def sparkwebhook():
     if request.method == 'POST':
 
@@ -50,15 +56,58 @@ def sparkwebhook():
         if message.personId == me.id:
             return 'OK'
         else:
-            words = message.text.split(' ')
+            string = message.text.replace(',', '')
+            string = string.replace('.', '')
+            string = string.lower()
+            words = string.split(' ')
             x=0
-            for i in words:
-                if(i in interaction):
-
-                    spark_api.messages.create(room.id, text=interaction[i])
-                    x=1
-            if(x==0):        
-                spark_api.messages.create(room.id, text='HELLO')
+            i=0
+            while (i<len(words) and x==0):
+                word = words[i]
+                if(type != []):
+                    if(word == 'yes'):
+                        type.remove(1)
+                        spark_api.messages.create(room.id, text=random.choice(interaction['another']))
+                        x=1
+                    elif(word == 'no'):
+                        spark_api.messages.create(room.id, text=random.choice(interaction['goodbye']))
+                        sys.exit(0)
+                else:
+                    if(word == 'yes'):
+                        if(interaction[word]!=[]):
+                            s=str(random.choice(interaction[word]))
+                            interaction[word].remove(s)
+                            x=str(random.choice(interaction[used[-1]]))
+                            query.append(x)
+                            interaction[used[-1]].remove(x)
+                            s=s+x
+                            spark_api.messages.create(room.id, text=s)
+                            x=1
+                    elif(word == 'no'):
+                            r = str(response[query[-1]])
+                            r = r.replace('[','')
+                            r = r.replace(']','')
+                            r = str(r[1:-1])
+                            r = r + ' '
+                            w = str(interaction['nextquery'])
+                            w = w.replace('[','')
+                            w = w.replace(']','')
+                            w = str(w[1:-1])
+                            r=r+w
+                            type.append(1)
+                            x=1
+                            spark_api.messages.create(room.id, text=r)
+                    elif(word in interaction):
+                        used.append(word)
+                        if(interaction[word]!=[]):
+                            s=random.choice(interaction[word])
+                            query.append(s)
+                            spark_api.messages.create(room.id, text=s)
+                            interaction[word].remove(s)
+                            x=1
+                    i=i+1
+            if(x==0):
+                spark_api.messages.create(room.id, text=random.choice(interaction['generic']))
 
     else:
         print('received none post request, not handled!')
